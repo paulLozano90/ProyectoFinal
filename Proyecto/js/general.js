9ac855a5c6c9eee3605d4f81a198f76463c60051
paulLozano90 <plc90@hotmail.es>
2015-04-28 22:45:48 +0200

    Archivos

---
+++ Proyecto/js/general.js
@@ -0,0 +1,17 @@
+
+
+//
+//$( document ).ready(function() {
+//    
+//    //$('.prueba').hide(duracion,callback);
+//  $(".btn-prueba").click(function () {
+//    $('.prueba').show(5,0);
+//    });
+//    
+//});
+
+$(document).ready(function(){
+    $("#bprueba").click(function(){
+        $("#prueba").slideDown("slow");
+    });
+});
