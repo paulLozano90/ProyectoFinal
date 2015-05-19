7fafb6093cbb67163a1870790a22512599e1abe6
danicb <dancar_madrid@hotmail.com>
2015-05-19 18:22:31 +0200

    tablas

---
+++ Proyecto/php/tablas.sql
@@ -0,0 +1,40 @@
+
+create database unete
+
+CREATE TABLE Empresa
+
+(id_empresa INT NOT NULL AUTO_INCREMENT,
+cif VARCHAR(20),
+nombre_empresa VARCHAR(20),
+descripcion VARCHAR(50),
+email VARCHAR(10),
+telefono INT(10),
+direccion VARCHAR(30),
+localidad VARCHAR(30),
+codigo_postal INT(5),
+ciudad VARCHAR(20))
+
+
+
+
+CREATE TABLE Usuarios 
+
+(id_usuario INT NOT NULL AUTO_INCREMENT,
+nombre VARCHAR(20),
+apellido1 VARCHAR(10),
+apellido2 VARCHAR(10),
+email VARCHAR(10),
+telefono INT(10),
+contraseña VARCHAR(10))
+
+
+CREATE TABLE Eventos
+ 
+(id_evento INT NOT NULL AUTO_INCREMENT
+id_empresa 
+
+
+)
+
+
+
