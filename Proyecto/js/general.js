bfc1fb047f865179899a1b6904caae2481d4d294
paulLozano90 <plc90@hotmail.es>
2015-05-19 15:41:48 +0200

    cambios

--- Proyecto/js/general.js
+++ Proyecto/js/general.js
@@ -1,13 +1,25 @@
+
+var ejemplos = [["../images/OFERTA.jpg","OFERTA","Descripcion oferta",10],
+                ["../images/OFERTA2.jpg","OFERTA2","Descripcion oferta2",20],
+                ["../images/OFERTA3.jpg","OFERTA3","Descripcion oferta3",30]];
+
 $(document).ready(function () {
-    if ($(document).height() > 991 || $(document).width() < 991)
-    {
+    if ($(document).height() > 991 || $(document).width() < 991){
         $("#footer").css("position", "absolute");
     }
     $("#abrir").click(function () {
         $("#formulario").slideDown("slow");
     });
+    
+    mostrarDatos();
 });
 
-
-
-
+function mostrarDatos(){
+ 
+    for (var i=0; i<ejemplos.length; i++){
+        for (var j=0; j<ejemplos.length; j++){
+            
+            alert(ejemplos[i][j]);
+        }
+    }
+}
