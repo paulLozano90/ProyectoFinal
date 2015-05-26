d16bdc09d58a1f397821d99361031fd10c418661
paulLozano90 <plc90@hotmail.es>
2015-05-26 17:48:37 +0200

    eventos de los botones creados

--- Proyecto/js/general.js
+++ Proyecto/js/general.js
@@ -1,64 +1,53 @@
 
-var ejemplos = [["images/OFERTA.jpg", "OFERTA", "Descripcion oferta", 10],
-    ["images/OFERTA2.jpg", "OFERTA2", "Descripcion oferta2", 20],
-    ["images/OFERTA3.jpg", "OFERTA3", "Descripcion oferta3", 30]];
 
-$(document).ready(function() {
+$(document).ready(function () {
+
     if ($(document).height() > 991 || $(document).width() < 991) {
         $("#footer").css("position", "absolute");
     }
+
+    //ocultar login para registrarse
     $("#abrir").click(function() {
         $("#formulario").show();
         $("#login").hide();
     });
+    //Ocultar la descripcion del evento
+    $(".btn-compra").click(function () {
+        $("#dResumen").show();
+        $("#dDescripcion").hide();
+        $("#cantidad").val("1");
+        $("#precioTotal")[0].innerHTML = $("#precioEvento")[0].innerHTML + " Euros";
 
-    mostrarDatos();
+    });
+    $(".btn-volver").click(function () {        
+        $("#dDescripcion").show();
+        $("#dResumen").hide();
+    });
+    $("#cantidad").change(function () {
+        
+        var cantidad = $("#cantidad").val();
+        var precio = $("#precioEvento")[0].innerHTML;
+
+        $("#precioTotal")[0].innerHTML = (cantidad * precio)+" Euros";        
+    });
+    //Enlace para mostrar la descripcion del evento
+    $(".btn-ofertas").click(function () {
+        //Redireccion cuando haya value...
+        //window.location.href = "http://localhost:50571/descripEvento.html?id=" + $(this).val() + "";
+        window.location.href = "http://localhost:50571/descripEvento.html";
+    });
 });
 
-function mostrarDatos() {
+//Funcion para sacar los parametros de la URL
+$.urlParam = function (elemento) {
 
-    $("#ofertas div")[1].children[0].src = ejemplos[0][0];
-    $("#ofertas div")[1].children[1].innerHTML = ejemplos[0][1];
-    $("#ofertas div")[1].children[2].innerHTML = ejemplos[0][2];
-    $("#ofertas div")[1].children[3].value = ejemplos[0][3];
+    var param = new RegExp('[\\?&]' + elemento + '=([^&#]*)');
+    var url = window.location.href;
+    var dato = param.exec(url);
 
-    $("#ofertas div")[2].children[0].src = ejemplos[1][0];
-    $("#ofertas div")[2].children[1].innerHTML = ejemplos[1][1];
-    $("#ofertas div")[2].children[2].innerHTML = ejemplos[1][2];
-    $("#ofertas div")[2].children[3].value = ejemplos[1][3];
-
-    $("#ofertas div")[3].children[0].src = ejemplos[2][0];
-    $("#ofertas div")[3].children[1].innerHTML = ejemplos[2][1];
-    $("#ofertas div")[3].children[2].innerHTML = ejemplos[2][2];
-    $("#ofertas div")[3].children[3].value = ejemplos[2][3];
-
-//    var contDiv = $("#ofertas div").length;
-//
-//    for (var i = 0; i < ejemplos.length; i++) {
-//        
-//        for (var j = 0; j < ejemplos.length + 1; j++) {
-//
-//            //alert(ejemplos[i][j]);
-//            z = 1;
-//
-//            while (z < 4) {
-//
-//                switch (j) {
-//                    case 0:
-//                        $("#ofertas div")[z].children[0].src = ejemplos[i][j];
-//                        break;
-//                    case 1:
-//                        $("#ofertas div")[z].children[1].innerHTML = ejemplos[i][j];
-//                        break;
-//                    case 2:
-//                        $("#ofertas div")[z].children[2].innerHTML = ejemplos[i][j];
-//                        break;
-//                    case 3:
-//                        $("#ofertas div")[z].children[3].value = ejemplos[i][j];
-//                        break;
-//                }
-//                z++;
-//            }
-//        }
-//    }
+    if (dato != null) {
+        return dato[1];
+    } else {
+        return dato;
+    }
 }
