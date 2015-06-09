559a4d1400c435613640e0fc30dd766af245daf4
paulLozano90 <plc90@hotmail.es>
2015-06-09 20:56:19 +0200

    fotos pruebas

--- Proyecto/pruebas/Fotos/foto.php
+++ Proyecto/pruebas/Fotos/foto.php
@@ -1,28 +1,29 @@
+
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <form action = "" method = "post" enctype="multipart/form-data">            
+            <fieldset>                
+                <label for="fotos">Foto (*):</label>
+                <input type="file" name="foto"><br><br>
+                <input type="submit"/>        
+            </fieldset>
+        </form>
+    </body>
+</html>
+
 <?php
+
+$foto = "";
 
 if (isset($_FILES["foto"]) and $_FILES["foto"]["error"] == UPLOAD_ERR_OK) {
 
-    //Si el archivo no es una foto JPEG, la funcion lo rechaza
-    if ($_FILES["foto"]["type"] != "image/jpeg") {
-        
-    } elseif (!move_uploaded_file($_FILES["foto"]["tmp_name"], "foto/" . basename($_FILES["foto"]["name"]))) {
+    if (!move_uploaded_file($_FILES["foto"]["tmp_name"], "C:/wamp/www/foto/" . basename($_FILES["foto"]["name"]))) {
         $foto = $_FILES["foto"]["name"];
     }
-} else {  
-
-    switch ($_FILES["foto"]["error"]) {
-
-        case UPLOAD_ERR_INI_SIZE:
-            
-            break;
-        case UPLOAD_ERR_FORM_SIZE:
-            
-            break;
-        case UPLOAD_ERR_NO_FILE:
-            
-            break;
-        default:
-    }
+    echo $_FILES["foto"]["name"];
 }
-
-echo $foto;
