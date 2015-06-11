e2cb5469bf0bc559215c36a89a51b6ad37e927c1
paulLozano90 <plc90@hotmail.es>
2015-06-11 16:04:42 +0200

    cookies

--- Proyecto/js/funciones.js
+++ Proyecto/js/funciones.js
@@ -24,8 +24,25 @@ Descripcion=
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
- * Funcion para sacar los parametros de la URL
+window.onload = function () {
+    cookies();
+    deleteAllCookies();
+};
+
+/* nombre:      nVentana
+ * descrpcion:  funcion que abre las ventanas emergentes de la web
+ * parametros:  ninguno
+ * devuelve:    nada
+ */
+
+function nVentana(link) {
+    var nv = window.open(link, "nventana", "scrollbars=yes, top=0, left=0, height=750px, width=600px");
+}
+
+/* nombre:      $urlParam
+ * descrpcion:  funcion que nos da los parametros de la url
+ * parametros:  elemento
+ * devuelve:    valor del parametro de la url
  */
      
 $.urlParam = function(elemento) {
@@ -38,5 +55,53 @@ $.urlParam = function(elemento) {
         return dato[1];
     } else {
         return dato;
+    }
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ')
+            c = c.substring(1);
+        if (c.indexOf(name) == 0)
+            return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+}
+
+function cookies() {
+    if (!getCookie("cookieHabilitado")) {        
+        $("#avisoCookies").show();
+        $("#avisoCookies").css("height", "33px");
+    }
+    else {
+        $("#avisoCookies").hide(1000);
+        alert("Cookies Aceptadas");
+    }
+}
+
+function aceptarCookie() {
+    $("#avisoCookies").hide(1000);
+    $("#avisoCookies").css("height", "0px");
+    setCookie("cookieHabilitado", true, 365);
+}
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
