7efa8a34372b878952ce51371a60205abd695115
paulLozano90 <plc90@hotmail.es>
2015-06-14 03:14:10 +0200

    insert cambiados

--- Proyecto/sql/insertTablas.sql
+++ Proyecto/sql/insertTablas.sql
@@ -4,66 +4,66 @@
 insert into usuarios (nombre,apellido1,apellido2,email,telefono,password) values 
 ('Paul','Lozano','Cruzado','plc90@hotmail.es',914125468,'123456');
 
-
 /*Insertar tipo de empresas */
 
-insert into tipoEvento (nombre) values ('Deportes');
 insert into tipoEvento (nombre) values ('Restaurantes');
-insert into tipoEvento (nombre) values ('Salud y belleza');
-
+insert into tipoEvento (nombre) values ('Ocio');
+insert into tipoEvento (nombre) values ('Salud y Belleza');
 
 /*Insertar empresas */
 
 insert into empresas (cif,nombre,descripcion,email,telefono,direccion,localidad,codPostal,ciudad) values 
-('45434543S','Deportes Moya','Empresa de deporte','deportesmoya@gmail.es',914125468,'Avenida de America Nº3','Madrid',28702,'Madrid');
-
-insert into empresas (cif,nombre,descripcion,email,telefono,direccion,localidad,codPostal,ciudad) values 
-('23332332S','Spa Relax and fun','Belleza y Salud','sparelax@gmail.es',914455468,'Calle del sueño Nº3','Alcobendas',28703,'Madrid');
-
-insert into empresas (cif,nombre,descripcion,email,telefono,direccion,localidad,codPostal,ciudad) values 
 ('78786743S','Restaurante Chistu','Restaurantes','chisturestaurante@hotmail.es',912325433,'Calle del hambre Nº21','Madrid',28700,'Madrid');
 
+insert into empresas (cif,nombre,descripcion,email,telefono,direccion,localidad,codPostal,ciudad) values 
+('45434543S','Multiaventuras Moya','Deportes','deportesmoya@gmail.es',914125468,'Avenida de las Americas Nº3','San Sebastian de los Reyes',28702,'Madrid');
 
-/*Insertar eventos deportivos */
+insert into empresas (cif,nombre,descripcion,email,telefono,direccion,localidad,codPostal,ciudad) values 
+('23332332S','Spa Relax and Fun','Belleza y Salud','sparelax@gmail.es',914455468,'Calle del sueño Nº3','Alcobendas',28703,'Madrid');
 
-insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion,precioNormal,precioReducido,fechaCreacion,fechaCaducidad) 
-values (1,1,'Paintball','Batalla de paintball','Batalla de paintball en la sierra de Madrid junto a tus amigos o enemigos',30,15,
-(STR_TO_DATE(REPLACE('18/01/2015','/','.') ,GET_FORMAT(date,'EUR'))),(STR_TO_DATE(REPLACE('18/03/2015','/','.') ,GET_FORMAT(date,'EUR'))));
+/*Insertar eventos */
 
-insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion,precioNormal,precioReducido,fechaCreacion,fechaCaducidad) 
-values (1,1,'Piraguas','Recorrido en piragüas por el Manzanares','Recorrido en piragüas por las tranquilas aguas del rio Manzanares',20,15,
-(STR_TO_DATE(REPLACE('10/04/2015','/','.') ,GET_FORMAT(date,'EUR'))),(STR_TO_DATE(REPLACE('10/05/2015','/','.') ,GET_FORMAT(date,'EUR'))));
+insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion, precioNormal,precioReducido,fechaCreacion,fechaCaducidad,foto) 
+values ('1','1','Menú Americano para compartir','Disfruta de un ambiente retro con este deliciso menú para dos con patatas y milkshake',
+'Si lo que te encanta es una sabrosa hamburguesa de buey, esta Oferta es para ti. Y si lo que te encanta es degustar los más variados y sorprendentes sándwiches, también',
+'35','17', '13/06/2015','01/07/2015','americano.jpg');
 
-insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion,precioNormal,precioReducido,fechaCreacion,fechaCaducidad) 
-values (1,1,'Karts','Carrera de Karts','Carrera de karts en circuito al aire libre',50,30,
-(STR_TO_DATE(REPLACE('25/05/2015','/','.') ,GET_FORMAT(date,'EUR'))),(STR_TO_DATE(REPLACE('25/07/2015','/','.') ,GET_FORMAT(date,'EUR'))));
+insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion, precioNormal,precioReducido,fechaCreacion,fechaCaducidad,foto) 
+values ('2','2','Partida de paintball','Disfruta de una partida de paintball con tus amigos',
+'Con esta oferta podras liberar toda tu adrenalina y demostrar que eres un auténtico soldado',
+'47','22', '13/06/2015','31/07/2015','paintball.jpg');
 
+insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion, precioNormal,precioReducido,fechaCreacion,fechaCaducidad,foto) 
+values ('3','3','Sesiones de Rayos UVA','Disfruta de 5 sesiones dobles de rayos UVA My Sun y presume de moreno sin haber ido a la playa.',
+'Consigue un bronceado dorado y luminoso incluso cuando no es verano. Además, hazlo sin exponerte al sol gracias a este fantástico descuento que te ofrecemos',
+'70','37', '13/06/2015','15/07/2015','rayos.jpg');
 
-/*Insertar eventos de restaurantes */
+insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion, precioNormal,precioReducido,fechaCreacion,fechaCaducidad,foto) 
+values ('1','1','Tapeo por Madrid','Tapeo para 2 personas y cubo de cervezas',
+'Sea la época del año que sea, tapear es siempre un placer, por eso, hoy hemos preparado esta exquisita y excelente Oferta de tapeo: molletes, cazuelitas, bebidas, ¡y con bebida incluida! ',
+'30','15', '13/06/2015','3/09/2015','tapas.jpg');
 
-insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion,precioNormal,precioReducido,fechaCreacion,fechaCaducidad) 
-values (2,2,'Menu Peruano','Menu degustacion Peruano','Disfruta de un gran menu Peruano incluyendo arroz estilo Paul',30,12,(STR_TO_DATE(REPLACE('20/07/2015','/','.') ,GET_FORMAT(date,'EUR'))),
-(STR_TO_DATE(REPLACE('20/09/2015','/','.') ,GET_FORMAT(date,'EUR'))));
+insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion, precioNormal,precioReducido,fechaCreacion,fechaCaducidad,foto) 
+values ('2','2','Partida de minigolf','Disfruta de una partida de minigolf en las mejores instalaciones',
+'Si eres un amante del golf  en busca de la diversión asegurada, esta Oferta es tu mejor opción para disfrutar con tu familia o tus amigos',
+'20','9', '13/06/2015','15/08/2015','minigolf.jpeg');
 
-insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion,precioNormal,precioReducido,fechaCreacion,fechaCaducidad) 
-values (2,2,'Restaurante El Pescador','Menú degustación deluxe en el Restaurante El Pescador','Menú degustación deluxe de cocina casera en el restaurante El Pescador',40,20,(STR_TO_DATE(REPLACE('25/08/2015','/','.') ,GET_FORMAT(date,'EUR'))),
-(STR_TO_DATE(REPLACE('25/10/2015','/','.') ,GET_FORMAT(date,'EUR'))));
+insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion, precioNormal,precioReducido,fechaCreacion,fechaCaducidad,foto) 
+values ('3','3','Circuito SPA y masaje','Te ofrecemos un circuito SPA de 120 min. y masaje oriental de 50 min',
+'Para alcanzar un relax total, un máximo bienestar, pasar un día ajeno al estrés te ofrecemos el refugio ideal. Una sesión de SPA de 2 horas completas individual o en pareja.',
+'65','35', '13/06/2015','25/08/2015','spa.jpg');
 
-insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion,precioNormal,precioReducido,fechaCreacion,fechaCaducidad) 
-values (2,2,'Desayunos con Encanto','Completo desayuno a domicilio con un gran descuento',' Hoy te proponemos una alternativa original, un estupendo desayuno o merienda a domicilio para 1 o 2 personas.',25,10,(STR_TO_DATE(REPLACE('25/10/2015','/','.') ,GET_FORMAT(date,'EUR'))),
-(STR_TO_DATE(REPLACE('25/12/2015','/','.') ,GET_FORMAT(date,'EUR'))));
+insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion, precioNormal,precioReducido,fechaCreacion,fechaCaducidad,foto) 
+values ('1','1','Menú peruano para dos','Escápate a tierras incas con el paladar con este sensacional menú',
+'Regálale a tu paladar un viaje gastronómico hasta Perú sin tener que hacer las maletas. Degusta este menú peruano para 2 personas',
+'65','20', '13/06/2015','18/08/2015','peruano.jpg');
 
+insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion, precioNormal,precioReducido,fechaCreacion,fechaCaducidad,foto) 
+values ('2','2','Tandas de karts','Si la velocidad te corre por las venas, enfundate el casco',
+'Podrás disfrutar de la máxima velocidad con karts .Siéntete como un auténtico profesional de la velocidad',
+'19','13', '13/06/2015','15/07/2015','karting.jpg');
 
-/*Insertar eventos de salud y belleza */
-
-insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion,precioNormal,precioReducido,fechaCreacion,fechaCaducidad) 
-values (3,3,'Clínica Capilar Laura','Tratamiento para contorno de ojos (bolsas y ojeras)','Ofrecen un tratamiento específico para mejorar el contorno de ojos, y aportarle luz a tu mirada, atenuando las arrugas y disminuyendo la apariencia de bolsas y ojeras.',55,20,(STR_TO_DATE(REPLACE('22/04/2015','/','.') ,GET_FORMAT(date,'EUR'))),
-(STR_TO_DATE(REPLACE('22/03/2015','/','.') ,GET_FORMAT(date,'EUR'))));
-
-insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion,precioNormal,precioReducido,fechaCreacion,fechaCaducidad) 
-values (3,3,'Centro internacional del láser II','Ecografía 4D, Vídeo y Canastilla de regalo','Te ponemos en bandeja la oportunidad de que puedas ver perfectamente la carita de tu bebé y sus movimientos a través de la ecografía en 4D gracias al Hello baby eco4',85,35,(STR_TO_DATE(REPLACE('10/04/2015','/','.') ,GET_FORMAT(date,'EUR'))),
-(STR_TO_DATE(REPLACE('10/04/2015','/','.') ,GET_FORMAT(date,'EUR'))));
-
-insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion,precioNormal,precioReducido,fechaCreacion,fechaCaducidad) 
-values (3,3,'Hotel Palacio de La Magdalena','Circuito termal en el Hotel Palacio de la Magdalena','Piscina dinámica con cascadas, masajes automáticos de chorros alternantes, tumbonas de burbujas y aeromasaje vertical, termas seca y húmeda, jacuzzi, pediluvios, duchas de contrastes, tumbonas calefactoras con cromoterapia',40,15,(STR_TO_DATE(REPLACE('12/05/2015','/','.') ,GET_FORMAT(date,'EUR'))),
-(STR_TO_DATE(REPLACE('12/06/2015','/','.') ,GET_FORMAT(date,'EUR'))));
+insert into eventos (idEmpresa,idTipo,nombre,descripCorta,descripcion, precioNormal,precioReducido,fechaCreacion,fechaCaducidad,foto) 
+values ('3','3','Depilación láser Unisex','Acaba con el vello de tu cuerpo con estas 3 sesiones de Depilación láser diodo',
+'¿Pelos tú? Hace tiempo que han desaparecido de tu cuerpo gracias al Láser de Diodo que, aunque parezca la varita mágica de Photoshop, se trata de la depilación definitiva para eliminar el vello.',
+'210','40', '13/06/2015','1/08/2015','depilacion.jpg');
