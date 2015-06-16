c798206ad939794dbdd2cf1e10037c8050cc37cb
paulLozano90 <plc90@hotmail.es>
2015-06-16 18:39:55 +0200

    usuario logueado para crearEvento

--- Proyecto/js/principalEmpresa.js
+++ Proyecto/js/principalEmpresa.js
@@ -1,35 +1,42 @@
 
 $(document).ready(function() {
-    
+
     var idEmpresa = 2;
-    muestraEventosEmpresa(idEmpresa,"idEvento#asc");
+    muestraEventosEmpresa(idEmpresa, "idEvento#asc");
     
-    $("#selectOrder").change(function() {        
-        muestraEventosEmpresa(idEmpresa,$("#selectOrder").val());
+    if($('#usuarioLogeado')[0].innerHTML === ""){
+        
+        var nombreUsuario = getCookie("usuarioLogeado");
+        nombreUsuario = nombreUsuario.replace("%20", " ");
+        $('#usuarioLogeado')[0].innerHTML = "&nbsp;" + nombreUsuario + "&nbsp;";
+    }    
+
+    $("#selectOrder").change(function() {
+        muestraEventosEmpresa(idEmpresa, $("#selectOrder").val());
     });
 });
 
-function muestraEventosEmpresa(idEmpresa,elemento){
-    
+function muestraEventosEmpresa(idEmpresa, elemento) {
+
     var url = "../php/principalEmpresa.php";
     var jSonvar = {idEmpresa: idEmpresa, elemento: elemento};
-    
+
     $.getJSON(url, jSonvar, function(evento) {
         $("#tablaMuestraTodos tbody").html("");
-        $.each(evento, function(i, evento) { 
+        $.each(evento, function(i, evento) {
             var fila = "<tr>"
                     + "<td>" + evento.nombreEvento + "</td>"
                     + "<td>" + evento.precioNormal + "</td>"
-                    + "<td>" + evento.precioReducido + "</td>" 
+                    + "<td>" + evento.precioReducido + "</td>"
                     + "<td>" + evento.fechaCreacion + "</td>"
                     + "<td>" + evento.fechaCaducidad + "</td>"
                     + "<td><button class='btn btn-default btn-evento' value='" + evento.idEvento + "'>Ver Evento</button></td>"
                     + "</tr>";
 
             $(fila).appendTo("#tablaMuestraTodos tbody");
-            $("#tablaMuestraTodos th:nth-child(1)").css("width","300px");
-            
-            $('#usuarioLogeado')[0].innerHTML = "&nbsp;"+evento.empresa+"&nbsp;";
+            $("#tablaMuestraTodos th:nth-child(1)").css("width", "300px");
+
+            $('#usuarioLogeado')[0].innerHTML = "&nbsp;" + evento.empresa + "&nbsp;";
             setCookie("usuarioLogeado", evento.empresa, 365);
 
             $(".btn-evento").click(function() {
